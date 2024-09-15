60947339a08785bd85570a6d910be21ced85219c
unknown <alexhparsons@gmail.com>
2024-09-15 15:07:29 -0700

    +

---
+++ Desktop/coding/equations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Please enter two positive integers, seperated by a space:";
+
+    int firstInteger, secondInteger;
+
+    cin >> firstInteger >> secondInteger;
+
+    int additionAnswer = firstInteger + secondInteger;
+    int subtractionAnswer = firstInteger - secondInteger;
+    int multiplicationAnswer = firstInteger * secondInteger;
+    float floatdivisionAnswer = float(firstInteger) / float(secondInteger);
+    int divisionAnswer = firstInteger / secondInteger;
+    int modAnswer = firstInteger % secondInteger;
+
+    cout << firstInteger << " + " << secondInteger << " = " << additionAnswer << endl;
+    cout << firstInteger << " - " << secondInteger << " = " << subtractionAnswer << endl;
+    cout << firstInteger << " * " << secondInteger << " = " << multiplicationAnswer << endl;
+    cout << firstInteger << " / " << secondInteger << " = " << floatdivisionAnswer << endl;
+    cout << firstInteger << " div " << secondInteger << " = " << divisionAnswer << endl;
+    cout << firstInteger << " mod " << secondInteger << " = " << modAnswer << endl;
+
+
+}
--- Desktop/coding/howmanycoins.cpp
+++ Desktop/coding/howmanycoins.cpp
@@ -8,11 +8,6 @@ const int PENNIES = 1;
 
 int main() {
 
-    int numberOfQuarters = 0;
-    int numberOfDimes = 0;
-    int numberOfNickles = 0;
-    int numberOfPennies = 0;
-
     int amountOfDollars, amountOfCents;
 
     cout << "Please enter your amount in the format of dollars and cents separated by a space\n";
@@ -21,21 +16,23 @@ int main() {
 
     cout << amountOfDollars << " dollars and " << amountOfCents << " cents are:\n";
 
+
     int totalCents = ((amountOfDollars * 100) + amountOfCents);
 
     int totalQuarters = totalCents / QUARTERS;
 
-    int totalLeft1 = totalCents - (totalQuarters * QUARTERS);
+    int totalLeft = totalCents - (totalQuarters * QUARTERS);
 
-    int totalDimes = totalLeft1 / DIMES;
+    int totalDimes = totalLeft / DIMES;
 
-    int totalLeft2 = totalLeft1 - (totalDimes * DIMES);
+    totalLeft = totalLeft - (totalDimes * DIMES);
 
-    int totalNickles = totalLeft2 / NICKLES;
+    int totalNickles = totalLeft / NICKLES;
 
-    int totalLeft3 = totalLeft2 - (totalNickles * NICKLES);
+    totalLeft = totalLeft - (totalNickles * NICKLES);
 
-    int totalPennies = totalLeft3 / PENNIES;
+    int totalPennies = totalLeft / PENNIES;
+
 
     cout << totalQuarters << " quarters " << totalDimes << " dimes " << totalNickles << " nickles " << totalPennies << " and pennies";
 
